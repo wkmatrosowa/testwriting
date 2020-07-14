7c1045af85defb4c557766b095cc574cdeb0d55a
wksmirnowa <merriel@inbox.ru>
2020-07-14 19:09:58 +0300

    edit tests for 6kyu task

--- 2020-07-14/task_test.py
+++ 2020-07-14/task_test.py
@@ -3,6 +3,10 @@ import unittest
 from task import order
 class OrderTest(unittest.TestCase):
 
+    def test_1(self):
+        result = order('begi4n th2e Le1t 3game')
+        self.assertEqual(result, 'Le1t th2e 3game begi4n')
+
     def test_2(self):
         result = order('чт2о попр4обовать э6то 5написать де7ло 1А кириллице8й? если3')
         self.assertEqual(result, '1А чт2о если3 попр4обовать 5написать э6то де7ло кириллице8й?')
@@ -15,9 +19,7 @@ class OrderTest(unittest.TestCase):
         result = order('wrong4 wi5th w2hat i6t Che1ck we3nt')
         self.assertEqual(result, 'Che1ck w2hat we3nt wrong4 wi5th i6t')
 
-    # def test_1(self):
-    #     result = order('begi4n th2e Le1t 3game')
-    #     self.assertEqual(result, 'Le1t th2e 3game begi4n')
+
 
 if __name__ == '__main__':
     unittest.main()
